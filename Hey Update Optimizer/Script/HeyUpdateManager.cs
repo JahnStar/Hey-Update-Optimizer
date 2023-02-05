cea8b6b4054639d9df6fad5ed33a352a730bc4f6
Halil Emre Yildiz <halilemreyildiz@gmail.com>
2023-02-05 17:44:53 +0300

    Update HeyUpdateManager.cs

--- Hey Update Optimizer/Script/HeyUpdateManager.cs
+++ Hey Update Optimizer/Script/HeyUpdateManager.cs
@@ -24,69 +24,80 @@ namespace JahnStar.Optimization
             }
         }
         //
-        public bool applyChanges;
+        public bool reload;
         [Header("Update Frequency")]
-        [Tooltip("Allows modification of 'updatePerFrame' runtime ignoring 'UpdatePerFrame' in IHeyUpdate.")]
-        public bool ignoreAllInterfaces = false;
+        public bool ignoreInterfaces = false;
         private bool _ignoreAllInterfaces;
-        [Tooltip("Determines how often the objects will be updated per frame. (Helps to optimize the update frequency)")]
         public int updatePerFrame = 2;
         private int _updatePerFrame;
         [Header("Pooling Ratio")]
-        [Tooltip("Allows modification 'processPerFrame'"), Range(0, 1)]
+        [Range(0, 1)]
         public float updatePoolingRatio = 0.5f;
-        public float _updatePoolingRatio;
-        [Tooltip("Determines the maximum number of objects that will be updated per frame. (Helps to optimize the update load, affects the actual frame rate)")]
-        private int processPerFrame = 0;
+        private float _updatePoolingRatio;
+        internal int processPerFrame = 0;
         //
-        private IHeyUpdate[] updatables;
+        internal IHeyUpdate[] updatables;
+        internal int _updatablesLength = 0;
         private int[] _updatePerFrameArray;
-        private int _updatablesLength, _processIndex = 0, _processCounter = 0, _frameCounter = 0, _counterLimit = 99999;
+        private int _processIndex = 0, _processCounter = 0, _frameCounter = 0, _counterLimit = 99999;
         private float _checkTime, _delayedTime;
+        private bool _update;
+        internal bool _editorInfo;
         public void SetProcessPerFrame(int processPerFrame) => _updatePoolingRatio = 1f - ((float)_updatablesLength / Mathf.Clamp(this.processPerFrame = processPerFrame, 1, _updatablesLength));
+        private void OnEnable()
+        {
+            _delayedTime = 0;
+            _checkTime = Time.time;
+        }
         private void Awake() => Load();
         internal void Load()
         {
+            _ignoreAllInterfaces = ignoreInterfaces;
+            _updatePerFrame = updatePerFrame;
+            _updatePoolingRatio = _ignoreAllInterfaces ? updatePoolingRatio : updatePoolingRatio = 0;
+            if (_updatePerFrame < 2) _updatePerFrame = 1;
+            //
             updatables = FindObjectsOfType<MonoBehaviour>().OfType<IHeyUpdate>().ToArray();
             _processIndex = _updatablesLength = updatables.Length;
             //
-            _ignoreAllInterfaces = ignoreAllInterfaces;
-            _updatePerFrame = updatePerFrame;
-            _updatePoolingRatio = updatePoolingRatio;
-            //
             processPerFrame = _updatePoolingRatio < 0.01f ? _updatablesLength : _updatePoolingRatio > 0.99f ? 1 : (int)(_updatablesLength * (1f - _updatePoolingRatio));
-            _updatePerFrameArray = updatables.Select(u => u.UpdatePerFrame).ToArray();
-            _counterLimit = _ignoreAllInterfaces ? _updatePerFrame : FindLCM(_updatePerFrameArray);
-            if (_updatePerFrame < 2) _updatePerFrame = 1;
-            applyChanges = false;
+            _updatePerFrameArray = updatables.Select(u => u.UpdatePerFrame < 2 ? 1 : u.UpdatePerFrame).ToArray();
+            _counterLimit = _ignoreAllInterfaces ? _updatePerFrame : FindLCM((int[])_updatePerFrameArray.Clone());
+            _frameCounter = 0;
+            reload = false;
         }
-        public static void ReLoad() => Instance.applyChanges = true;
+        public static void ReLoad() => Instance.reload = true;
         private void Update()
         {
             if (_processIndex >= _updatablesLength)
             {
                 _frameCounter++;
-                //
-                float time = Time.time;
-                _delayedTime = time - _checkTime;
-                _checkTime = time;
-                //
-                if (applyChanges) Load();
                 _processIndex = _processCounter = 0;
             }
+            if (_ignoreAllInterfaces) _update = Updatable(_updatePerFrame);
             if (_frameCounter >= _counterLimit) _frameCounter = 0;
             while (_processIndex < _updatablesLength)
             {
                 try
                 {
-                    if (!_ignoreAllInterfaces)
+                    if (!_ignoreAllInterfaces) _update = Updatable(_updatePerFrameArray[_processIndex]);
+                    if (_processIndex == 0 && _update)
                     {
-                        _updatePerFrame = _updatePerFrameArray[_processIndex];
-                        if (_updatePerFrame < 2) _updatePerFrame = 1;
+                        if (reload)
+                        {
+                            Load();
+                            return;
+                        }
+                        else
+                        {
+                            float time = Time.time;
+                            _delayedTime = time - _checkTime;
+                            _checkTime = time;
+                        }
                     }
-                    if (Updatable(_updatePerFrame)) updatables[_processIndex].HeyUpdate(_updatePerFrame * _delayedTime);
+                    if (_update) updatables[_processIndex].HeyUpdate(_delayedTime);
                 }
-                catch { applyChanges = true; return; }
+                catch (Exception e) { reload = true; Debug.LogWarning(e.Message); return; }
                 _processIndex++;
                 _processCounter++;
                 if (_processCounter >= processPerFrame)
